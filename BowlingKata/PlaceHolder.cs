bf4aa6d0449c07dcc645648c3d32c09d6073e72a
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-10-08 20:33:57 +0100

    Refactorings

---
+++ BowlingKata/PlaceHolder.cs
@@ -0,0 +1,19 @@
+﻿namespace BowlingKata
+{
+    internal class PlaceHolder
+    {
+        public PlaceHolder(int frameNumber)
+        {
+            FrameNumber = frameNumber;
+        }
+
+        public PlaceHolder(int score, int frameNumber)
+        {
+            Score = score;
+            FrameNumber = frameNumber;
+        }
+
+        public int? Score { get; private set; }
+        public int FrameNumber { get; private set; }
+    }
+}
--- BowlingKata/TenPinBowlingCalculator.cs
+++ BowlingKata/TenPinBowlingCalculator.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 
 namespace BowlingKata
 {
@@ -7,52 +6,44 @@ namespace BowlingKata
     {
         public int Calculate(params int[] rolls)
         {
-            var total = 0;
-            var frameCount = 1;
-            int? previousRoll = null;
+            var frameNumber = 1;
             var placeHolders = new List<PlaceHolder>();
+            int? previousRoll = null;
+            var total = 0;
 
             foreach (var roll in rolls)
             {
-                if (frameCount > 1)
+                if (frameNumber > 1)
                 {
-                    var placeHolder = placeHolders.FirstOrDefault(ph => ph.Score == -1);
-                    if (placeHolder != null)
-                    {
-                        placeHolder.Score = roll;
-                    }
+                    total += FillInPlaceHolder(placeHolders, roll);
                 }
 
-                if (frameCount > 2)
+                if (frameNumber > 2)
                 {
-                    var placeHolder = placeHolders.FirstOrDefault(ph => ph.Score == -1);
-                    if (placeHolder != null)
-                    {
-                        placeHolder.Score = roll;
-                    }
+                    total += FillInPlaceHolder(placeHolders, roll);
                 }
 
-                if (frameCount <= 10)
+                if (frameNumber <= 10)
                 {
                     total += roll;
 
-                    if (roll == 10)
+                    if (RollIsAStrike(roll))
                     {
-                        placeHolders.Add(new PlaceHolder(frameCount));
-                        placeHolders.Add(new PlaceHolder(frameCount));
+                        placeHolders.Add(new PlaceHolder(frameNumber));
+                        placeHolders.Add(new PlaceHolder(frameNumber));
                         previousRoll = null;
-                        frameCount++;
+                        frameNumber++;
                     }
                     else
                     {
                         if (previousRoll.HasValue)
                         {
-                            if (previousRoll + roll == 10)
+                            if (PreviousRollAndThisRollMakeASpare(previousRoll, roll))
                             {
-                                placeHolders.Add(new PlaceHolder(frameCount));
+                                placeHolders.Add(new PlaceHolder(frameNumber));
                             }
                             previousRoll = null;
-                            frameCount++;
+                            frameNumber++;
                         }
                         else
                         {
@@ -62,21 +53,30 @@ namespace BowlingKata
                 }
             }
 
-            total += placeHolders.Sum(ph => ph.Score);
-
             return total;
         }
 
-        private class PlaceHolder
+        private static bool RollIsAStrike(int roll)
         {
-            public int Score { get; set; }
-            public int FrameNumber { get; private set; }
+            return roll == 10;
+        }
 
-            public PlaceHolder(int frameNumber)
+        private static bool PreviousRollAndThisRollMakeASpare(int? previousRoll, int roll)
+        {
+            return previousRoll + roll == 10;
+        }
+
+        private static int FillInPlaceHolder(List<PlaceHolder> placeHolders, int roll)
+        {
+            var index = placeHolders.FindIndex(PlaceHolderExtensions.IsEmpty);
+
+            if (index < 0)
             {
-                Score = -1;
-                FrameNumber = frameNumber;
+                return 0;
             }
+
+            placeHolders[index] = placeHolders[index].With(roll);
+            return roll;
         }
     }
 }

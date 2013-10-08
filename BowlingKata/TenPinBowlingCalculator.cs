4e0edadd6fe79de54fc6ba54308dee1605e19cde
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-10-08 19:36:37 +0100

    Initial commit

---
+++ BowlingKata/TenPinBowlingCalculator.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BowlingKata
+{
+    public class TenPinBowlingCalculator
+    {
+        public int Calculate(params int[] rolls)
+        {
+            var total = 0;
+            var frameCount = 1;
+            int? previousRoll = null;
+            var placeHolders = new List<PlaceHolder>();
+
+            foreach (var roll in rolls)
+            {
+                if (frameCount > 1)
+                {
+                    var placeHolder = placeHolders.FirstOrDefault(ph => ph.Score == -1);
+                    if (placeHolder != null)
+                    {
+                        placeHolder.Score = roll;
+                    }
+                }
+
+                if (frameCount > 2)
+                {
+                    var placeHolder = placeHolders.FirstOrDefault(ph => ph.Score == -1);
+                    if (placeHolder != null)
+                    {
+                        placeHolder.Score = roll;
+                    }
+                }
+
+                if (frameCount <= 10)
+                {
+                    total += roll;
+
+                    if (roll == 10)
+                    {
+                        placeHolders.Add(new PlaceHolder(frameCount));
+                        placeHolders.Add(new PlaceHolder(frameCount));
+                        previousRoll = null;
+                        frameCount++;
+                    }
+                    else
+                    {
+                        if (previousRoll.HasValue)
+                        {
+                            if (previousRoll + roll == 10)
+                            {
+                                placeHolders.Add(new PlaceHolder(frameCount));
+                            }
+                            previousRoll = null;
+                            frameCount++;
+                        }
+                        else
+                        {
+                            previousRoll = roll;
+                        }
+                    }
+                }
+            }
+
+            total += placeHolders.Sum(ph => ph.Score);
+
+            return total;
+        }
+
+        private class PlaceHolder
+        {
+            public int Score { get; set; }
+            public int FrameNumber { get; private set; }
+
+            public PlaceHolder(int frameNumber)
+            {
+                Score = -1;
+                FrameNumber = frameNumber;
+            }
+        }
+    }
+}
---
+++ BowlingKataTests/TenPinBowlingCalculatorTests.cs
@@ -0,0 +1,102 @@
+﻿using System.Linq;
+using BowlingKata;
+using NUnit.Framework;
+
+namespace BowlingKataTests
+{
+    // http://codingdojo.org/cgi-bin/wiki.pl?KataBowling
+
+    [TestFixture]
+    internal class TenPinBowlingCalculatorTests
+    {
+        private TenPinBowlingCalculator _tenPinBowlingCalculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _tenPinBowlingCalculator = new TenPinBowlingCalculator();
+        }
+
+        [Test]
+        public void EmptySequenceGivesZero()
+        {
+            var actual = _tenPinBowlingCalculator.Calculate();
+            Assert.That(actual, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void SequenceOfOneGivesOne()
+        {
+            var actual = _tenPinBowlingCalculator.Calculate(1);
+            Assert.That(actual, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void SequenceOfOneOneGivesTwo()
+        {
+            var actual = _tenPinBowlingCalculator.Calculate(1, 1);
+            Assert.That(actual, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void SequenceOfSpareThenTwoMoreRollsGivesCorrectTotal()
+        {
+            // Triangulation
+
+            var actual1 = _tenPinBowlingCalculator.Calculate(3, 7, 1, 2);
+            Assert.That(actual1, Is.EqualTo((3 + 7 + 1) + (1 + 2)));
+
+            var actual2 = _tenPinBowlingCalculator.Calculate(5, 5, 3, 4);
+            Assert.That(actual2, Is.EqualTo((5 + 5 + 3) + (3 + 4)));
+        }
+
+        [Test]
+        public void SequenceOfStrikeThenTwoMoreRollsGivesCorrectTotal()
+        {
+            var actual = _tenPinBowlingCalculator.Calculate(10, 3, 4);
+            Assert.That(actual, Is.EqualTo((10 + 3 + 4) + (3 + 4)));
+        }
+
+        [Test]
+        public void SequenceOfTenPairsOfNineAndAMissGives90()
+        {
+            var actual = _tenPinBowlingCalculator.Calculate(9, 0, 9, 0, 9, 0, 9, 0, 9, 0, 9, 0, 9, 0, 9, 0, 9, 0, 9, 0);
+            Assert.That(actual, Is.EqualTo(90));
+        }
+
+        [Test]
+        public void SequenceOfTenPairsOfFiveAndFiveWithFinalFiveGives150()
+        {
+            var actual = _tenPinBowlingCalculator.Calculate(Enumerable.Repeat(5, 21).ToArray());
+            Assert.That(actual, Is.EqualTo(150));
+        }
+
+        [Test]
+        public void SequenceOfTenPairsOfFiveAndFiveWithFinalSevenGives152()
+        {
+            var actual = _tenPinBowlingCalculator.Calculate(Enumerable.Repeat(5, 20).Concat(new[] {7}).ToArray());
+            Assert.That(actual, Is.EqualTo(152));
+        }
+
+        [Test]
+        public void SequenceOfStrikeThenSpareThenOneMoreRollGivesCorrectTotal()
+        {
+            var actual = _tenPinBowlingCalculator.Calculate(10, 8, 2, 5);
+            Assert.That(actual, Is.EqualTo((10 + 8 + 2) + (8 + 2 + 5) + (5)));
+        }
+
+        [Test]
+        public void SequenceOfStrikeThenStrikeThenOneTwoGivesCorrectTotal()
+        {
+            var actual = _tenPinBowlingCalculator.Calculate(10, 10, 1, 2);
+            Assert.That(actual, Is.EqualTo((10 + 10 + 1) + (10 + 1 + 2) + (1 + 2)));
+        }
+
+        [Test]
+        public void SequenceOf12StrikesGives300()
+        {
+            var actual = _tenPinBowlingCalculator.Calculate(Enumerable.Repeat(10, 12).ToArray());
+            Assert.That(actual, Is.EqualTo(300));
+        }
+    }
+}

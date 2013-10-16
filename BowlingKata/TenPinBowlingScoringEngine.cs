fee2cff2ba6b878155767c4a51c71273d09ac689
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-10-16 19:26:56 +0100

    Extracted a couple of methods to make ProcessRolls clearer

--- BowlingKata/TenPinBowlingScoringEngine.cs
+++ BowlingKata/TenPinBowlingScoringEngine.cs
@@ -6,33 +6,6 @@ namespace BowlingKata
 {
     public class TenPinBowlingScoringEngine
     {
-        public Frame[] ProcessRolls(IEnumerable<int> rolls)
-        {
-            var frames = MakeTenEmptyFrames();
-
-            foreach (var roll in rolls)
-            {
-                foreach (var index in Enumerable.Range(0, 10))
-                {
-                    var rollBelongsToThisFrame = frames[index].ProcessRoll(roll);
-                    if (rollBelongsToThisFrame)
-                    {
-                        break;
-                    }
-                }
-            }
-
-            var runningTotal = 0;
-            foreach (var index in Enumerable.Range(0, 10))
-            {
-                var frame = frames[index];
-                runningTotal += frame.Score;
-                frame.SetRunningTotal(runningTotal);
-            }
-
-            return frames;
-        }
-
         public int CalculateTotalScore(IEnumerable<int> rolls)
         {
             return ProcessRolls(rolls).Sum(frame => frame.Score);
@@ -52,17 +25,50 @@ namespace BowlingKata
             return sb.ToString();
         }
 
+        public Frame[] ProcessRolls(IEnumerable<int> rolls)
+        {
+            var frames = MakeTenEmptyFrames();
+            PopulateFrames(rolls, frames);
+            SetRunningTotals(frames);
+            return frames;
+        }
+
+        private static Frame[] MakeTenEmptyFrames()
+        {
+            return Enumerable.Range(1, 10).Select(frameNumber => new Frame(frameNumber)).ToArray();
+        }
+
+        private static void PopulateFrames(IEnumerable<int> rolls, IList<Frame> frames)
+        {
+            foreach (var roll in rolls)
+            {
+                foreach (var frame in frames)
+                {
+                    var rollBelongsToThisFrame = frame.ProcessRoll(roll);
+                    if (rollBelongsToThisFrame)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        private static void SetRunningTotals(IEnumerable<Frame> frames)
+        {
+            var runningTotal = 0;
+            foreach (var frame in frames)
+            {
+                runningTotal += frame.Score;
+                frame.SetRunningTotal(runningTotal);
+            }
+        }
+
         private static void AppendRollSymbol(StringBuilder sb, Roll roll)
         {
             if (roll != null)
             {
                 sb.Append(roll.Symbol);
             }
-        }
-
-        private static Frame[] MakeTenEmptyFrames()
-        {
-            return Enumerable.Range(1, 10).Select(frameNumber => new Frame(frameNumber)).ToArray();
         }
     }
 }
